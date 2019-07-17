a0e0efcb1efd2df557a94c528b74c7c2c353c158
manches <manches@172.20.1.55>
2019-07-17 10:12:58 +0200

    asdasd

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -40,7 +40,6 @@ import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.storage.StorageLevel;
-import org.apache.spark.mllib.rdd.RDDFunctions;
 
 import scala.Tuple2;
 
