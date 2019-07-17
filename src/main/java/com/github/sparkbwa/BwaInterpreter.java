32a23a1d2752e7502507dc7f6a3e5d1eb70255ba
manches <manches@172.20.1.55>
2019-07-17 15:06:40 +0200

    asdasd

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -263,7 +263,8 @@ public class BwaInterpreter {
 		//dfFinal = this.sparkSession.createDataset(this.ctx.textFile(options.getInputPath()).sliding(4))
 		//		.toDF("identifier", "sequence", "quality");
 		JavaRDD<String> rAUX1 = this.ctx.textFile(options.getInputPath());
-		RDD<Object> r1 = RDDFunctions.fromRDD(rAUX1.rdd(), rAUX1.classTag()).sliding(4,4).map(case Array(id, seq, _, qual) -> (id, seq, qual));
+		RDD<Object> r1 = RDDFunctions.fromRDD(rAUX1.rdd(), rAUX1.classTag())
+				.sliding(4,4)∫;
 		//dfFinal = this.sparkSession.createDataset( rAUX1.rdd().sliding(4,4)).toDF();
 				
 				//.map {
