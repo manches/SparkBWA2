f202c977ef08372d4c5c8c09076ee0579a3831fb
manches <manches@172.20.1.55>
2019-07-17 14:28:21 +0200

    asdad

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -259,7 +259,7 @@ public class BwaInterpreter {
 		JavaPairRDD<Long, String> datasetTmp2 = loadFastq(this.ctx, options.getInputPath2());
 		JavaPairRDD<Long, Tuple2<String, String>> pairedReadsRDD = datasetTmp1.join(datasetTmp2);
 		
-		dfFinal = this.sparkSession.createDataset(this.ctx.textFile(options.getInputPath())).sliding(4, 4).map{
+		dfFinal = this.sparkSession.createDataset(this.ctx.textFile(options.getInputPath()).sliding(4, 4).map{
 		 case Array(id, seq, _, qual) => (id, seq, qual)
 		}).toDF("identifier", "sequence", "quality");
 		
