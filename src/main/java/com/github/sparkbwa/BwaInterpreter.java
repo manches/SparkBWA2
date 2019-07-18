ee13888d844c15b351274b1c8542de7efdf4841e
manches <manches@172.20.1.55>
2019-07-18 12:22:37 +0200

    asdas

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -17,6 +17,7 @@
 package com.github.sparkbwa;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
