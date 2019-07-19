4aa3bb83b24036d5f96af6ec735c8b8e9c82282c
manches <manches@172.20.1.55>
2019-07-19 14:33:19 +0200

    asad

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -453,7 +453,7 @@ public class BwaInterpreter {
 		
 		
 		return readsDS
-				.mapPartitions(new BwaPairedAlignmentDS(this.sparkSession.sparkContext(), bwa),R owEncoder.apply(readsDS.schema()) )
+				.mapPartitions(new BwaPairedAlignmentDS(this.sparkSession.sparkContext(), bwa),RowEncoder.apply(readsDS.schema()) )
 				.collect();
 		
 	/*	
