83f1a736d01f9bed6d4f6086b7f3b841fd0f8c03
manches <manches@172.20.1.5>
2020-04-21 12:16:28 +0200

    asd

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -57,7 +57,17 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import org.apache.hadoop.io.IOUtils;
-
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
