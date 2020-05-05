3d4ad4471c292ddf29d8350a56cd0ebaf3e49940
manches <manches@172.20.1.5>
2020-05-05 18:59:09 +0200

    123123

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -260,7 +260,15 @@ public class BwaInterpreter {
 				   .map((Function<String, Row>) record -> {
 			      String[] parts = record.split("\n");
 			      //return RowFactory.create(attributes[0], attributes[1].trim());
-			      return RowFactory.create("@"+parts[0].trim(),parts[1].trim(),parts[2].trim(),parts[3].trim());
+			      if (sTexto.startsWith("@")) {
+			    	  return RowFactory.create(parts[0].trim(),parts[1].trim(),parts[2].trim(),parts[3].trim());  
+			      } else {
+			    	  return RowFactory.create("@"+parts[0].trim(),parts[1].trim(),parts[2].trim(),parts[3].trim());
+			      }
+			     
+			      
+			      
+			      
 			    });
 		      // collect RDD for printing
 	        for(String line:filteredJavaRDD.collect()){
