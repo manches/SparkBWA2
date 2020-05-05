2c1a0ae1f298f35acdde8fbca62d89adc38ef6e5
manches <manches@172.20.1.5>
2020-05-05 22:55:05 +0200

    kkj

--- src/main/java/com/github/sparkbwa/TextInputFormat.java
+++ src/main/java/com/github/sparkbwa/TextInputFormat.java
@@ -16,10 +16,18 @@
  */
 package com.github.sparkbwa;
 
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TextInputFormat extends FileInputFormat<LongWritable, Text> {
 	 
