fdf575fa62b59e3a3b786dc3eda4c88a6bf26130
manches <manches@172.20.1.5>
2020-05-12 23:12:32 +0200

    asdd

---
+++ src/main/java/com/github/sparkbwa/TextInputFormat.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2016 José Manuel Abuín Mosquera <josemanuel.abuin@usc.es>
+ *
+ * <p>This file is part of SparkBWA.
+ *
+ * <p>SparkBWA is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * <p>SparkBWA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * <p>You should have received a copy of the GNU General Public License along with SparkBWA. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package com.github.sparkbwa;
+public class TextInputFormat extends FileInputFormat<LongWritable, Text> {
+
+  @Override
+  public RecordReader<LongWritable, Text> 
+    createRecordReader(InputSplit split,
+                       TaskAttemptContext context) {
+    String delimiter = context.getConfiguration().get(
+        "textinputformat.record.delimiter");
+    byte[] recordDelimiterBytes = null;
+    if (null != delimiter)
+      recordDelimiterBytes = delimiter.getBytes(Charsets.UTF_8);
+    return new LineRecordReader(recordDelimiterBytes);
+  }
+//这个对文件做压缩用的
+  @Override
+  protected boolean isSplitable(JobContext context, Path file) {
+    final CompressionCodec codec =
+      new CompressionCodecFactory(context.getConfiguration()).getCodec(file);
+    if (null == codec) {
+      return true;
+    }
+    return codec instanceof SplittableCompressionCodec;
+  }
+
+}
