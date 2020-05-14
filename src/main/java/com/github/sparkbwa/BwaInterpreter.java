b8672f352b3214bf0a949ec45c671c47dac759d0
manches <manches@172.20.1.5>
2020-05-14 23:43:36 +0200

    kjkljl

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -879,7 +879,7 @@ public class BwaInterpreter {
 		//The Hadoop configuration is obtained    
 		this.conf = this.ctx.hadoopConfiguration();
         this.conf.setBoolean("fs.hdfs.impl.disable.cache", true);
-		this.conf.set("textinputformat.record.delimiter","\u0098@");
+		this.conf.set("textinputformat.record.delimiter","\n@");
 
 		//The block size
 		this.blocksize = this.conf.getLong("dfs.blocksize", 134217728);
