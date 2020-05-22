821b75172625e2f12acab5dc62765a6ed83f756d
manches <manches@172.20.1.5>
2020-05-22 23:26:10 +0200

    asda

--- src/main/java/com/github/sparkbwa/BwaInterpreter.java
+++ src/main/java/com/github/sparkbwa/BwaInterpreter.java
@@ -426,10 +426,11 @@ public class BwaInterpreter {
 		JavaRDD<Row> result = x.map(new Function<Object,Row>() {
 			@Override
 			public Row call(Object arg0) throws Exception {
-				  String text = Arrays.toString((Object[])arg0);
-			      String[] parts = text.substring(1, text.length()-1).split(",");
-
-		    	return RowFactory.create(parts[0].trim(),parts[1].trim(),parts[2].trim(),parts[3].trim());
+				List<> mlist =  (List) Object;
+				return RowFactory.create(mlist.toArray());
+			//	  String text = Arrays.toString((Object[])arg0);
+			//      String[] parts = text.substring(1, text.length()-1).split(",");
+		    //	return RowFactory.create(parts[0].trim(),parts[1].trim(),parts[2].trim(),parts[3].trim());
 
 			}
 		});	     
